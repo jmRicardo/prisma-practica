ae46bb037c0708e22fec8feb28c51af1923968ea
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-03-04 20:03:58 -0300

    Preparando el proyecto para el servicio

---
+++ src/main/java/com/moby/prisma/jmr/prismapractica/PrismaPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.moby.prisma.jmr.prismapractica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrismaPracticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrismaPracticaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moby/prisma/jmr/prismapractica/PrismaPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moby.prisma.jmr.prismapractica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrismaPracticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
